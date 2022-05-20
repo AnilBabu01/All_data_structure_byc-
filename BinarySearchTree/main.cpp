76ea20804eb85b19a375f9d02bc94ce51582bef0
anil babu <anilb9850@gmail.com>
2022-05-20 10:47:58 +0530

    create and check Isbst

---
+++ BinarySearchTree/main.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+
+using namespace std;
+class node{
+   public:
+   int data;
+   node *left;
+   node *right;
+
+     node(int val)
+     {
+         data=val;
+         left=NULL;
+         right=NULL;
+     }
+
+
+};
+class Traversals{
+
+  public:
+      void PreOrder(node* root){
+       if(root!=NULL)
+       {
+           cout<<root->data<<" ";
+           PreOrder(root->left);
+           PreOrder(root->right);
+       }
+
+     }
+      void PostOrder(node* root){
+       if(root!=NULL)
+       {
+           PostOrder(root->left);
+           PostOrder(root->right);
+           cout<<root->data<<" ";
+
+       }
+    }
+      void InOrder(node* root){
+       if(root!=NULL)
+       {
+
+           InOrder(root->left);
+           cout<<root->data<<" ";
+           InOrder(root->right);
+       }
+
+     }
+
+        int IsBST(node* root)
+        {
+          static  node* pre = NULL;
+          if(root!=NULL)
+          {
+             if(IsBST(root->left))
+             if(pre!=NULL && root->data <=pre->data)
+             {
+                 return 0;
+             }
+            pre=root;
+            return IsBST(root->right);
+          }
+          else
+          {
+
+              return 1;
+          }
+
+       }
+};
+int main()
+{
+
+   Traversals traversals;
+   node *p = new node(5);
+   node *p1 = new node(3);
+   node *p2 = new node(6);
+   node *p3 = new node(1);
+   node *p4 = new node(4);
+//        5
+//        /\
+//       3  6
+//       /\
+//      1  4
+//
+
+   p->left=p1;
+   p->right=p2;
+   p1->left=p3;
+   p1->right=p4;
+  cout<<"PreOder traversal "<<endl;
+  traversals.PreOrder(p);
+  cout<<endl;
+  cout<<"PostOder traversal "<<endl;
+  traversals.PostOrder(p);
+  cout<<endl;
+  cout<<"InOder traversal "<<endl;
+  traversals.InOrder(p);
+
+  cout<<endl;
+    cout<<traversals.IsBST(p);
+
+    return 0;
+}
